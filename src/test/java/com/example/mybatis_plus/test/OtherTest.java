34668f9de43e12472e4a13ff0780015a57db5a1f
fqq <2998657166@qq.com>
2024-03-09 20:27:29 +0800

    添加 lambda 方法引用获取属性名的测试用例

--- src/test/java/com/example/mybatis_plus/test/OtherTest.java
+++ src/test/java/com/example/mybatis_plus/test/OtherTest.java
@@ -1,12 +1,19 @@
 package com.example.mybatis_plus.test;
 
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.core.toolkit.LambdaUtils;
+import com.baomidou.mybatisplus.core.toolkit.support.LambdaMeta;
+import com.baomidou.mybatisplus.core.toolkit.support.SFunction;
+import com.baomidou.mybatisplus.core.toolkit.support.SerializedLambda;
+import com.baomidou.mybatisplus.core.toolkit.support.ShadowLambdaMeta;
 import com.example.mybatis_plus.domain.User;
 import com.example.mybatis_plus.mapper.UserMapper;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -14,6 +21,9 @@ import java.util.List;
 
 @SpringBootTest
 class OtherTest {
+
+    private static final Logger log = LoggerFactory.getLogger(OtherTest.class);
+
     @Autowired
     UserMapper userMapper;
 
@@ -25,5 +35,43 @@ class OtherTest {
         queryWrapper.eq(User::getName, "lisi");
         List<User> users = userMapper.selectList(queryWrapper);
         assertNotEquals(0, users.size());
+    }
+
+    @Test
+    void utilTest() {
+        LambdaMeta extract = LambdaUtils.extract(Customer::getName);
+        String methodName = extract.getImplMethodName();
+        log.info("method reference name: {}", methodName);
+        assertEquals("getName", methodName);
+    }
+
+    @Test
+    void serializeExtractTest() {
+        ShadowLambdaMeta shadowLambdaMeta = new ShadowLambdaMeta(SerializedLambda.extract(
+            (SFunction<Customer, String>) Customer::getName));
+        String methodName = shadowLambdaMeta.getImplMethodName();
+        log.info("method reference name: {}", methodName);
+        assertEquals("getName", methodName);
+    }
+
+    static class Customer {
+        private String name;
+        private Integer age;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public Integer getAge() {
+            return age;
+        }
+
+        public void setAge(Integer age) {
+            this.age = age;
+        }
     }
 }
